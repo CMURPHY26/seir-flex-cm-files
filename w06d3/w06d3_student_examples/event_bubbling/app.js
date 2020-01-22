c0c3e7fee5b494ecf5c1d96e39e744d88f678623
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-21 21:50:02 -0500

    finished event bubbling lesson

--- w06d3/w06d3_student_examples/event_bubbling/app.js
+++ w06d3/w06d3_student_examples/event_bubbling/app.js
@@ -3,18 +3,25 @@ console.log('app.js');
 $(() => {
   
     $(".outside").on("click", (event) => {
-        console.log("OUTSIDE target is: ", event.target);
-        console.log("OUTSIDE currentTarget is: ", event.currentTarget);
+        // console.log("OUTSIDE target is: ", event.target);
+        // console.log("OUTSIDE currentTarget is: ", event.currentTarget);
+        $(event.target).css("background-color", "black");
+        // $(event.currentTarget).css("background-color", "black");
     });
 
-    $(".inside").on("click", (event) => {
-        console.log("INSIDE target is: ", event.target);
-        console.log("INSIDE currentTarget is: ", event.currentTarget);
-    });
 
-    $("body").on("click", (event) => {
-        console.log("BODY target is: ", event.target);
-        console.log("BODY currentTarget is: ", event.currentTarget);
-    });
+
+
+    // $(".inside").on("click", (event) => {
+    //     console.log("INSIDE target is: ", event.target);
+    //     console.log("INSIDE currentTarget is: ", event.currentTarget);
+    //     // event.stopPropagation();
+    //     return false;//jquery shortcut to stop propogation;
+    // });
+
+    // $("body").on("click", (event) => {
+    //     console.log("BODY target is: ", event.target);
+    //     console.log("BODY currentTarget is: ", event.currentTarget);
+    // });
   
 });
