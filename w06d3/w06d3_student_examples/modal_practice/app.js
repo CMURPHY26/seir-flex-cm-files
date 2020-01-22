4eb13b359cb8b7fb84f724e53376e1e086206b17
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-21 19:42:36 -0500

    added event listeners for buttons

--- w06d3/w06d3_student_examples/modal_practice/app.js
+++ w06d3/w06d3_student_examples/modal_practice/app.js
@@ -2,5 +2,28 @@
 // console.log('modal practice app.js is linked to this index.html');
 
 $( () => {
-  //write your code here
+
+  // Grabbing About the Game button
+  const $openBtn = $("#openModal");
+  // Grabbing modal element
+  const $modal = $("#modal");
+  // Grabbing close button
+  const $closeBtn = $("#close");
+
+//Create an event handler to display the modal. This needs to be placed above the listener.
+  const openModal = () => {
+    $modal.show();
+  }
+
+  //Add event listener to About the Game button
+  $openBtn.on("click", openModal);
+
+  // Create an event handler to close the modal.
+  const closeModal = () => {
+    $modal.hide()
+  }
+//Add event listener to Close button
+  $closeBtn.on("click", closeModal)
+
+
 });
