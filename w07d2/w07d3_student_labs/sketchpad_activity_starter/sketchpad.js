5798c6079f0bffed6f686fc719c6fc2b3ea36d9d
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-28 18:31:27 -0500

    created w07d3 folders

--- w07d2/w07d3_student_labs/sketchpad_activity_starter/sketchpad.js
+++ w07d2/w07d3_student_labs/sketchpad_activity_starter/sketchpad.js
@@ -1,10 +1,34 @@
+let numberOfBoxes;
+
 $(()=>{
-    const changeColor = (e)=>{
-        $(e.target).addClass('pink');
+
+   
+    //CREATE variables for inputBox and submit (inputButton)
+    let $inputBox = $("#inputBox");
+    let $submit = $("#inputButton");
+
+    const createBoxes = () => {
+        for (var i = 0; i < numberOfBoxes; i++) {
+            const square = $('<div class="square">')
+                .appendTo('body')
+                .on('mouseover', changeColor);
+        }
     }
-    for (var i = 0; i < 1000; i++) {
-        const square = $('<div class="square">')
-            .appendTo('body')
-            .on('mouseover', changeColor);
+
+    //CREATE function to get the value in the input box
+    const storeNumber = () => {
+        console.log("submit clicked");
+        console.log($inputBox.val());
+        numberOfBoxes = Number($inputBox.val());
+        
+        createBoxes();
     }
+
+    $submit.on("click", storeNumber);
+
+    const changeColor = (e) =>{
+        $(e.target).addClass('orange');
+    }
+    
+
 });
