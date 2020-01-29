3aaeccd2d28188698809083a8f1fbadd88b4fab9
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-28 21:22:34 -0500

    completed first ajax request

---
+++ w07d3/w07d3_student_examples/ajax_movie_search/app.js
@@ -0,0 +1,49 @@
+
+console.log("It works!")
+
+
+$(() => {
+
+    ///OLD syntax
+    // $.ajax({
+    //     url: "http://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen",
+    //     success: (data) => {
+    //         console.log(data);
+    //     },
+    //     error: () => {
+    //         console.log("an error occurred")
+    //     }
+    // });
+
+
+    // ///PROMISES - newer syntax
+    // const promise = $.ajax({
+    //     url: "http://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen"
+    // });
+
+    // promise.then((data) => {
+    //         console.log(data);
+    //     },
+    //     () => {
+    //         console.log("Something went wrong");
+    //     }
+    // )
+
+    ///PROMISE with dot chain then
+    const promise = $.ajax({
+        url: "http://www.omdbapi.com/?apikey=53aa2cd6&t=inception"
+    }).then((data) => {
+            console.log(data);
+            $('#title').html(data.Title);
+            $('#year').html(data.Year);
+            $('#rated').html(data.Rated);
+
+        },
+        () => {
+            console.log("Something went wrong");
+        }
+    );
+
+
+
+});
