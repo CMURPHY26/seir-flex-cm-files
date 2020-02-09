df7b8b107f57e0b14d60bf87a3d888d5f1ad9a4b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 09:53:52 -0500

    set up .env file and linked variables

--- w09d2/w09d2_student_labs/express_calculator/server.js
+++ w09d2/w09d2_student_labs/express_calculator/server.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, ()=>{
+    console.log('app is running on port: ', port);
+})
 
 //to call .env variables use process.env.Variable 
 //where variable is what your calling from the .env file
