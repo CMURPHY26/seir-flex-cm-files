82c96e8bdd4f1484a250a36a2f2b6f721a45acab
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 10:34:48 -0500

    finished the server calculator functions

--- w09d2/w09d2_student_labs/express_calculator/server.js
+++ w09d2/w09d2_student_labs/express_calculator/server.js
@@ -8,11 +8,32 @@ const app = express();
 const port = process.env.PORT;
 
 
-app.get("/calc/:num1/:num2", (req, res) => {
-    console.log(req.params);
-    let sum = Number(req.params.num1) + Number(req.params.num2);
+// app.get("/calc/:num1/:num2", (req, res) => {
+//     console.log(req.params);
+//     let sum = Number(req.params.num1) + Number(req.params.num2);
     
-    res.send("the sum is:" + sum);
+//     res.send("the sum is:" + sum);
+// });
+
+app.get("/calc/:num1/:num2", (req, res) => {
+    console.log("req.query is:", req.query);
+
+    if( req.query.operation === "add") {
+        let sum = Number(req.params.num1) + Number(req.params.num2);
+        res.send("the sum is: " + sum);
+    } else if( req.query.operation === "subtract") {
+        let difference = Number(req.params.num1) - Number(req.params.num2);
+        res.send("The difference is: " + difference)
+    } else if(req.query.operation === "multiply") {
+        let product  = Number(req.params.num1) * Number(req.params.num2);
+        res.send("The product is: " + product);
+    } else if( req.query.operation === "divide") {
+        let quotient = Number(req.params.num1) / Number(req.params.num2);
+        res.send("The quotient is: " + quotient);
+    } else {
+        res.send("no operation");
+    };
+    
 });
 
 
