2109d8d74eb2a4ca30993bd2acfdcb258d11e720
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 10:08:02 -0500

    used params to make a server ccalculator

--- w09d2/w09d2_student_labs/express_calculator/server.js
+++ w09d2/w09d2_student_labs/express_calculator/server.js
@@ -1,3 +1,5 @@
+//to call .env variables use process.env.Variable 
+//where variable is what your calling from the .env file
 require('dotenv').config();
 
 const express = require('express');
@@ -5,9 +7,16 @@ const app = express();
 
 const port = process.env.PORT;
 
+
+app.get("/calc/:num1/:num2", (req, res) => {
+    console.log(req.params);
+    let sum = Number(req.params.num1) + Number(req.params.num2);
+    
+    res.send("the sum is:" + sum);
+});
+
+
+
 app.listen(port, ()=>{
     console.log('app is running on port: ', port);
-})
-
-//to call .env variables use process.env.Variable 
-//where variable is what your calling from the .env file
+});
