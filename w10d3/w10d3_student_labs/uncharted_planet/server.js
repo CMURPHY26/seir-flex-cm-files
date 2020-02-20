a995fea6f0d51ba953443ee43fdc2262c8f43b85
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-19 21:04:23 -0500

    updated exobilogy lab 1

--- w10d3/w10d3_student_labs/uncharted_planet/server.js
+++ w10d3/w10d3_student_labs/uncharted_planet/server.js
@@ -5,33 +5,53 @@ const app = express();
 const port = process.env.PORT;
 const methodOverride = require("method-override");
 //MODELS NOTE: it must start with ./ if it's just a file, not an NPM package
-const scientists = require("./models/scientists.js")
+let scientists = require("./models/scientists.js")
 
 //Tell express to use the middleware to process/access data
+//Load body parser middleware
 app.use(express.urlencoded({extended:true}));
+//Load methodOverride middleware
 app.use(methodOverride("_method"));
 
 
-app.get("/planet", (req, res) => {
-    res.render("index.ejs", {scientists: scientists});
-});
-
+//NEW
 app.get("/planet/new", (req, res) => {
     res.render("new.ejs");
 });
 
-app.post("/", (req, res) => {
-    // console.log(req.body);
-    scientists.push(req.body);
-    // console.log(scientists);
-    res.redirect("/planet");
+//SHOW
+app.get("/planet/:id", (req, res) =>{
+    res.render("show.ejs", {scientist: scientists[req.params.id]});
 });
 
-
+//DESTROY
 app.delete("/planet/:id", (req, res) => {
-    scientists.splice(scientists[req.params.id], 1);
+    scientists.splice(req.params.id, 1);
     res.redirect("/planet");
 });
+
+//CREATE
+app.post("/planet", (req, res) => {
+    // console.log(req.body);
+    const index = scientists.push(req.body) - 1;
+    // console.log(scientists);
+    res.redirect(`/planet/${index}`);
+});
+
+
+//DESTROY
+app.delete("/planet/", (req, res) => {
+    scientists.splice(0, scientists.length);
+    // scientists = [];
+    res.redirect("/planet");
+});
+
+//INDEX
+app.get("/planet", (req, res) => {
+    res.render("index.ejs", {scientists: scientists});
+});
+
+
 
 
 
