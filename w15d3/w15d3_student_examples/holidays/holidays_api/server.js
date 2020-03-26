26fe63f6e7a2d08ea02d31c42d5864d97c3206a2
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-03-25 20:55:04 -0400

    added cors code to remove CORS error

--- w15d3/w15d3_student_examples/holidays/holidays_api/server.js
+++ w15d3/w15d3_student_examples/holidays/holidays_api/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
+const cors = require('cors')
 const holidaysController = require('./controllers/holidays.js');
 
 
@@ -25,6 +26,27 @@ mongoose.connection.once('open', ()=>{
 //MIDDLEWARE
 //need to load the json to use it for the routes so add before controllers
 app.use(express.json());//use .json(), not .urlencoded()
+
+//NEED TO LOAD THE CORS MIDDLEWARE BEFORE ROUTES
+const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
+//For production and a secure environment, do this:
+// app.use(cors(corsOptions)) // Note: all routes are now exposed. If you want to limit access for specific verbs like POST or DELETE you can look at the npm documentaion for cors (for example with OMDB - it's ok for anyone to see the movies, but you don't want just anyone adding a movie)
+
+//But to Allow EVERYONE, do this:
+app.use(cors());//Allows everyone. Use with caution, only for dev, not for real apps
+
+
 //Mount the routes from holidays
 app.use('/holidays', holidaysController);
 
