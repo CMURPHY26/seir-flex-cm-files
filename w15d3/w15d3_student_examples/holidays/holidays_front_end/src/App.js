bc8ab5b686f30fb20c1afc2308a444997b8f956b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-03-28 13:07:25 -0400

    added update route for celebrated key

--- w15d3/w15d3_student_examples/holidays/holidays_front_end/src/App.js
+++ w15d3/w15d3_student_examples/holidays/holidays_front_end/src/App.js
@@ -3,7 +3,8 @@ import './css/normalize.css'
 import './css/skeleton.css'
 import './css/index.css'
 import NewForm from './components/NewForm'
-// import ballons from './images/two-balloon-icons-68911.png'
+import Show from './components/Show'
+import ballons from './images/two-balloon-icons-68911.png'
 // import pencil from './images/simpleiconDOTcom-pen-15-64x64.png'
 // import Show from './components/Show.js'
 // import UpdateForm from './components/UpdateForm.js'
@@ -32,9 +33,15 @@ console.log('current base URL:', baseURL)
 class App extends React.Component {
   // store the data made via fetch
   state = {
-    holidays: []
+    holidays: [],
+    // holiday: {}
   }
 
+  getHoliday = (holiday) => {
+    this.setState({holiday});
+  }
+
+  //why use fat arrows here?...ans: to bind to the class
   getHolidays = () => {
     fetch(baseURL+ '/holidays')
   .then(data => {
@@ -55,7 +62,39 @@ class App extends React.Component {
       holidays: copyHolidays,
       name: ''
     })
+  }
 
+  deleteHoliday = (id) => {
+    fetch(baseURL + '/holidays/' + id, {
+      method: 'DELETE'
+    }).then(response => {
+      //THE BELOW OR USE FILTER which is easier
+      // const findIndex = this.state.holidays.findIndex(holiday => holiday._id === id)
+      // const copyHolidays = [...this.state.holidays]
+      // copyHolidays.splice(findIndex, 1)
+      //holidaysArr = [{}, {}, {}]
+      const holidaysArr = this.state.holidays.filter( holiday => {
+        return holiday._id !== id
+      })
+      this.setState({holidays: holidaysArr})
+    })
+  }
+
+  toggleCelebrated = (holiday) => {
+    //holidays.put("/:id")
+    fetch(baseURL + '/holidays/' + holiday._id, {
+      method: 'PUT',
+      body: JSON.stringify({celebrated: !holiday.celebrated}),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    }).then(res => res.json())
+    .then(resJson => {
+         const copyHolidays = [...this.state.holidays]
+          const findIndex = this.state.holidays.findIndex(holiday => holiday._id === resJson._id)
+          copyHolidays[findIndex].celebrated = resJson.celebrated
+          this.setState({holidays: copyHolidays})
+    })
   }
 
 
@@ -69,12 +108,23 @@ class App extends React.Component {
       handleAddHoliday={this.handleAddHoliday}
       />
       <table>
+        {/* Conditional logic to show the active holiday  */}
+      {/* OPTION 1 show the active holiday*/}
+      {this.state.holiday ? <Show holiday={this.state.holiday} /> : null }
+      {/* OPTION 2 both below must be true to show  */}
+      {/* {this.state.holiday && <Show holiday={this.state.holiday} />} */}
         <tbody>
-
       {this.state.holidays.map( holiday => {
         return (
-          <tr key={holiday._id}>
+          <tr key={holiday._id} onMouseOver={() => this.getHoliday(holiday)}>
           <td>{holiday.name}</td>
+          <td>{holiday.likes}</td>
+          <td onDoubleClick={() => this.toggleCelebrated(holiday)}
+            className={holiday.celebrated
+              ? 'celebrated'
+              :
+              null}><img src={ballons} alt="ballons"/></td>
+          <td onClick={() =>this.deleteHoliday(holiday._id)}>X</td>
           </tr>
         )
       })}
