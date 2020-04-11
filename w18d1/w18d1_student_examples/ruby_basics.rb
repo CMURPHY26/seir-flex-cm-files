8a5c37f7ea58822a7854fd0f09d654896d55778a
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-11 11:30:36 -0400

    working with ruby variables and prompts with gets

---
+++ w18d1/w18d1_student_examples/ruby_basics.rb
@@ -0,0 +1,20 @@
+# "#" starts single comment in Ruby
+
+#Most variables use snake_case naming conventions
+long_variable_name = 1
+
+#The equivalent of console.log() is puts
+# Functions in ruby don't need parentheses, but you can use them
+
+# puts ('hi')
+# puts 'hi'
+# p 'hi'
+# p 1
+
+#We can also retrieve values from user input using gets
+# some_var = gets
+# puts some_var
+
+# command_line = gets
+# puts command_line
+
