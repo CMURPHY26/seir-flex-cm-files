3c796550ccf71ccb8a53f670635a2dc51f82ecaf
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-18 14:56:31 -0400

    finished sql lab part 2 televisions

--- w19d1/w19d1_student_labs/computers_television/televisions.sql
+++ w19d1/w19d1_student_labs/computers_television/televisions.sql
@@ -4,23 +4,35 @@
 -- Write SQL commands under the prompts below, and run the file to get results.
 
 -- In case there is already a televisions table, drop it
-
+    DROP TABLE televisions;
+    DROP TABLE tv_models;
 
 -- Create a televisions table
-
+    CREATE TABLE televisions (id serial, model_name varchar(20), screen_size int, resolution int, price int, release_date varchar(20), photo_url varchar(100));
 
 --  The table should have id, model_name, screen_size, resolution,
 --  price, release_date, photo_url
 
 
 -- Insert 4 televisions into the tv_models table
-
+    INSERT INTO televisions (model_name, screen_size, resolution, price, release_date, photo_url)
+    VALUES
+    ('Samsung', 55, 1080, 1500, '2020-03-15', 'https://tst.com/photo.jpg'),
+    ('Vizio', 65, 1080, 900, '2020-02-20', 'https://tst.com/photo.jpg'),
+    ('Sony', 560, 1080, 1000, '2019-12-15', 'https://tst.com/photo.jpg'),
+    ('Samsung', 65, 1080, 1800, '2020-01-13', 'https://tst.com/photo.jpg');
 
 -- Select all entries from the tv_models table
-
+    SELECT * FROM televisions;
 
 -- HUNGRY FOR MORE? 
 -- Look at this afternoon's instructor notes and read on altering tables before attempting below
 
 -- Alter the tv_models, removing the resolution column
 --  and add vertical_resolution and horizontal_resolution columns
+ALTER TABLE televisions RENAME TO tv_models;
+ALTER TABLE tv_models DROP COLUMN resolution;
+ALTER TABLE tv_models ADD COLUMN vertical_resolution int;
+ALTER TABLE tv_models ADD COLUMN horizontal_resolution int;
+
+SELECT * FROM tv_models
