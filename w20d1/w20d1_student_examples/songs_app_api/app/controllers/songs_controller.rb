8fce649b3e55c52514fa7d2d5d850d0822b6db11
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 11:38:03 -0400

    updated song controller to render json of all songs on index page

--- w20d1/w20d1_student_examples/songs_app_api/app/controllers/songs_controller.rb
+++ w20d1/w20d1_student_examples/songs_app_api/app/controllers/songs_controller.rb
@@ -1,7 +1,10 @@
 class SongsController < ApplicationController
+    def index
+        #long form syntax to connect directly into the database through ActiveRecord so you can issue SQL commands
+        render json: ActiveRecord::Base.connection.execute('SELECT * FROM songs;')
+        render json: Song.all
+    end
 
 end
 
-def index
-end
 
