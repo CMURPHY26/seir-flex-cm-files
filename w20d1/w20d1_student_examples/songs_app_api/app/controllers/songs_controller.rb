abf5136936209c1f8f38e688a55e0642a6a9076d
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 11:50:56 -0400

    created show route with params[:id]

--- w20d1/w20d1_student_examples/songs_app_api/app/controllers/songs_controller.rb
+++ w20d1/w20d1_student_examples/songs_app_api/app/controllers/songs_controller.rb
@@ -1,8 +1,20 @@
 class SongsController < ApplicationController
     def index
         #long form syntax to connect directly into the database through ActiveRecord so you can issue SQL commands
-        render json: ActiveRecord::Base.connection.execute('SELECT * FROM songs;')
-        render json: Song.all
+        # render json: ActiveRecord::Base.connection.execute('SELECT * FROM songs;')
+
+        # shorter syntax to use Object Relation Mapper
+        # render json: Song.all
+
+        #If we are going to deliver our API to some end-user, we should provide a status code.
+        #Change the code to deliver a 200 OK status code:
+        songs = Song.all
+        render json: {status: 200, songs: songs}
+    end
+
+    def show
+        #in javascript it was req.params.id instead of params[:id]
+        render json: Song.find(params[:id])
     end
 
 end
