45a2373e61335786b3a55746c3dd16026b16a65d
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 12:55:05 -0400

    added preview route

--- w20d1/w20d1_student_labs/books_app_api/app/controllers/books_controller.rb
+++ w20d1/w20d1_student_labs/books_app_api/app/controllers/books_controller.rb
@@ -10,5 +10,9 @@ class BooksController < ApplicationController
         render json: {status: 200, book: book}
     end
 
+    def preview
+        book = Book.find(params[:id])
+        render json: {status: 200, book: book.title, author: book.author}
+    end
 
 end
--- w20d1/w20d1_student_labs/books_app_api/config/routes.rb
+++ w20d1/w20d1_student_labs/books_app_api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
+
+  resources :books do
+    get 'preview', on: :member 
+  end
 end
