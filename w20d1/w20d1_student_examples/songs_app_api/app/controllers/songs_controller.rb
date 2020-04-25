35750679bae2f216857631ee3969974565ab17ee
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 11:30:50 -0400

    added songs controller and index

---
+++ w20d1/w20d1_student_examples/songs_app_api/app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+class SongsController < ApplicationController
+
+end
+
+def index
+end
+
--- w20d1/w20d1_student_examples/songs_app_api/config/routes.rb
+++ w20d1/w20d1_student_examples/songs_app_api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #this line resources :routes creates all routes
+  resources :songs
 end
