c1699b1488cfd17fb919fdce53236e24bf6e41b8
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 10:55:03 -0400

    added database seed loop

--- w20d1/w20d1_student_examples/songs_app_api/db/seeds.rb
+++ w20d1/w20d1_student_examples/songs_app_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+    Song.create(
+        title: Faker::Hipster.sentence(3),
+        artist_name: Faker::Team.name,
+        artwork: Faker::Placeholdit.image("50x50")
+    )
+end
