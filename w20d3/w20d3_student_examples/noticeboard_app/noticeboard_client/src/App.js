76585af9a35695baba6fc72dfe7507559c1ba5d2
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-29 19:51:33 -0400

    added rack cors and allowed localhost3001 to connect to localhost3000

--- w20d3/w20d3_student_examples/noticeboard_app/noticeboard_client/src/App.js
+++ w20d3/w20d3_student_examples/noticeboard_app/noticeboard_client/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import Aside from './components/Aside'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import Nav from './components/Nav'
 
 class App extends React.Component {
+  state = {
+    notices: []
+  }
+
+  componentDidMount() {
+    this.getNotices()
+  }
+
+  getNotices = () => {
+    //changed local host to 3001 because we are 
+    //using proxy in package json -- only use for development. You can also use relative urls and fetch "/notices" instead of below
+    fetch('http://localhost:3000/notices')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <h1>Notices</h1>
+        <div className='container'>
+          <Header />
+          <Aside />
+          <Main notices={this.state.notices} />
+          <Nav />
+          <Footer />
+        </div>
       </div>
     )
   }
