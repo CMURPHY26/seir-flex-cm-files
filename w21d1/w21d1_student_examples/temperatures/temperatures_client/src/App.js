2b114d8d08abdf6b18d0e2fbe2c978a43d2abd0f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-02 16:16:05 -0400

    used chart js and set up functions to build chart with data from api

--- w21d1/w21d1_student_examples/temperatures/temperatures_client/src/App.js
+++ w21d1/w21d1_student_examples/temperatures/temperatures_client/src/App.js
@@ -2,14 +2,8 @@ import React, { Component } from 'react';
 import BarChart from './components/BarChart.js'
 import './App.css';
 
-class App extends Component {
 
-  componentDidMount() {
-    fetch("/locations")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-  }
+class App extends Component {
 
   render() {
     return (
--- w21d1/w21d1_student_examples/temperatures/temperatures_client/src/components/BarChart.js
+++ w21d1/w21d1_student_examples/temperatures/temperatures_client/src/components/BarChart.js
@@ -2,6 +2,46 @@ import React, { Component } from 'react';
 import Chart from 'chart.js'
 
 class BarChart extends Component {
+    prepareData = (data) => {
+        const chartData = {
+          labels: [],
+          datasets: [
+            {
+              label: 'Avg high temps',
+              data: []
+            }
+          ]
+        }
+      
+        data.temperatures.forEach(temperature => {
+          chartData.labels.push(temperature.month)
+          chartData.datasets[0].data.push(temperature.average_high_f)
+        })
+    
+        return chartData
+      }
+    
+      createChart = (data) => {
+        const ctx = document.querySelector('#temperatures')
+        const tempsChart = new Chart(ctx, {
+          type: 'line',
+          data: data
+        })
+      }
+    
+      getData = () => {
+        fetch("/locations/1")
+        .then(response => response.json())
+        .then(jData => this.prepareData(jData))
+        .then(data => this.createChart(data))
+        .catch(err => console.log(err))
+      }
+    
+      componentDidMount() {
+        this.getData()
+      }
+
+
   render () {
     return (
       <>
