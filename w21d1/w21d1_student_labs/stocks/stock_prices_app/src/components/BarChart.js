da7376d763411a7a782930ff2303447b3dd43f04
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-05 19:07:20 -0400

    created rails api app with db created

--- w21d1/w21d1_student_labs/stocks/stock_prices_app/src/components/BarChart.js
+++ w21d1/w21d1_student_labs/stocks/stock_prices_app/src/components/BarChart.js
@@ -2,6 +2,47 @@ import React, { Component } from 'react'
 import Chart from 'chart.js'
 
 class BarChart extends Component {
+
+    getData = () => {
+        fetch("/stocks/1")
+        .then(response => response.json())
+        .then(jData => this.prepareData(jData))
+        .then(data => this.createChart(data))
+        .catch(err => console.log(err))
+    }
+
+    prepareData = (data) => {
+        const chartData = {
+            labels: [],
+            datasets: [
+              {
+                label: 'Avg price',
+                data: []
+              }
+  
+            ]
+          }
+        data.prices.forEach(price => {
+            chartData.labels.push(price.created_at);
+            chartData.datasets[0].data.push(price.avg_price)
+        })
+
+        return chartData
+
+    }
+
+    createChart = (data) => {
+        const ctx = document.querySelector('#stockPrices')
+	    const pricesChart = new Chart(ctx, {
+		type: 'line',
+		data: data
+			})
+    }
+
+    componentDidMount () {
+        this.getData();
+    }
+
     render() {
         return(
             <> 
